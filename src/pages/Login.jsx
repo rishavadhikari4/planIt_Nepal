f85a9f0fefa5bc31de840aacd7e2c71818c408f7
rishavadhikari4 <rishavadhikari657@gmail.com>
2025-05-08 19:43:28 +0545

    added the btn to go to the redirect

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,11 +27,41 @@ const Login = () => {
                 <h2>Login to Lagan Gaatho</h2>
                 {error && <p className="error-message">{error}</p>}
                 <form onSubmit={handleSubmit}>
-                    <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                     <button type="submit">Login</button>
                 </form>
-                <p>Don't have an account? <Link to="/register">Register</Link></p>
+                <div className="or-divider">
+                    <span>or</span>
+                </div>
+                <button
+                    type="button"
+                    className="google-login-btn"
+                    onClick={() =>
+                        window.location.href =
+                            "https://wedding-planner-backend-drr8.onrender.com/api/auth/google/"
+                    }
+                >
+                    <img
+                        src="https://developers.google.com/identity/images/g-logo.png"
+                        alt="Google"
+                        className="google-logo"
+                    />
+                    <span>Login with Google</span>
+                </button>
+                <p>
+                    Don't have an account? <Link to="/register">Register</Link>
+                </p>
             </div>
         </div>
     );
