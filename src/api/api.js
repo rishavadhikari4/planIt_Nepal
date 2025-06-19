5e341a4f87acf14e23eff0e927d167710601faa0
rishavadhikari4 <rishavadhikari657@gmail.com>
2025-06-19 15:17:41 +0545

    added the mistake api

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://192.168.1.73:5000",
+    // baseURL: "http://192.168.1.73:5000",
     // baseURL: "http://localhost:5000",
-    // baseURL:"https://wedding-planner-backend-drr8.onrender.com",
+    baseURL:"https://wedding-planner-backend-drr8.onrender.com",
     headers: {
         "Content-Type": "application/json"
     }
