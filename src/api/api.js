90dd7917a09cee374fbd75fde837b817bea495ae
rishavadhikari4 <rishavadhikari657@gmail.com>
2025-06-19 15:34:10 +0545

    correction api

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 const API = axios.create({
-    // baseURL: "http://192.168.1.73:5000",
-    // baseURL: "http://localhost:5000",
     baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
         "Content-Type": "application/json"
